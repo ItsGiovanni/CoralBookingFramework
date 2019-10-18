aad3ab746c7491e60b6a8a15671a5b5e03579e74
Giovanni <giovannidogger@gmail.com>
2019-10-18 12:07:33 +0200

    Pushing project to GIT

---
+++ app/Yacht Type.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Yacht Type extends Model
+{
+    //
+}
---
+++ app/Yacht_Type.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Yacht_Type extends Model
+{
+    //
+}
